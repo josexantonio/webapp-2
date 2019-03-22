73af39fa42c59399d265bb3f4a663475fb5d9322
oscarsoto <oscarsotosanchez@gmail.com>
2019-03-22 14:10:33 +0100

    adapted tests to elastest and fixed error angular

--- AMICOServer/src/test/java/com/example/demo/e2e/TestE2EFront.java
+++ AMICOServer/src/test/java/com/example/demo/e2e/TestE2EFront.java
@@ -2,6 +2,7 @@ package com.example.demo.e2e;
 
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -9,77 +10,48 @@ import static org.slf4j.LoggerFactory.getLogger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.hamcrest.text.IsEqualIgnoringCase;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 
 import io.github.bonigarcia.seljup.SeleniumExtension;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 @ExtendWith(SeleniumExtension.class)
-public class TestE2EFront extends TestE2E {
+public class TestE2EFront extends ElastestBaseTest {
 
 	final static Logger log = getLogger(lookup().lookupClass());
 
 	final static String PATH_DOWNLOAD = Paths.get(System.getProperty("user.dir"), "download-temporal").toString();
 
-	private Browser browser;
-
-	public TestE2EFront() {
-		WebDriverManager.chromedriver().setup();
-
-		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("profile.default_content_settings.popups", 0);
-		chromePrefs.put("download.default_directory", PATH_DOWNLOAD);
-
-		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("prefs", chromePrefs);
-		WebDriver driver = new ChromeDriver(options);
-
-		driver.manage().window().setSize(new Dimension(1024, 724));
-		this.browser = new Browser(driver, "http://localhost:4200/");
-	}
-
-	@AfterEach
-	public void dispose() {
-		this.browser.goToPage();
-		this.logout(browser);
-		
-		this.browser.getDriver().quit();
-	}
-
 	@Test
-	public void checkCreateCourse() {
+	public void checkCreateCourse(TestInfo testInfo) {
 		// Login
-		this.browser.goToPage("login");
-		this.loginUser(this.browser, "admin", "pass");
+		goToPage("login");
+		this.loginUser("admin", "pass");
 
 		// Create course
-		this.browser.goToPage("admin/addCourse");
+		goToPage("admin/addCourse");
 
-		WebElement name = browser.getDriver().findElement(By.name("newName"));
-		WebElement startDate = browser.getDriver().findElement(By.name("startDate"));
-		WebElement endDate = browser.getDriver().findElement(By.name("endDate"));
-		WebElement newLanguage = browser.getDriver().findElement(By.name("newLanguage"));
-		WebElement newType = browser.getDriver().findElement(By.name("newType"));
-		WebElement newSkill = browser.getDriver().findElement(By.name("newSkill1"));
-		WebElement newDescription = browser.getDriver().findElement(By.name("newDescription"));
-		WebElement image = browser.getDriver().findElement(By.name("courseImage"));
+		WebElement name = driver.findElement(By.name("newName"));
+		WebElement startDate = driver.findElement(By.name("startDate"));
+		WebElement endDate = driver.findElement(By.name("endDate"));
+		WebElement newLanguage = driver.findElement(By.name("newLanguage"));
+		WebElement newType = driver.findElement(By.name("newType"));
+		WebElement newSkill = driver.findElement(By.name("newSkill1"));
+		WebElement newDescription = driver.findElement(By.name("newDescription"));
+		WebElement image = driver.findElement(By.name("courseImage"));
 
-		WebElement divForm = browser.getDriver().findElement(By.className("form-group"));
+		WebElement divForm = driver.findElement(By.className("form-group"));
 		WebElement submit = divForm.findElement(By.tagName("button"));
 
 		String imageUpload = System.getProperty("user.dir") + "/src/test/resources/test.jpeg";
@@ -94,12 +66,12 @@ public class TestE2EFront extends TestE2E {
 		image.sendKeys(imageUpload);
 		submit.click();
 
-		browser.waitUntil(ExpectedConditions.visibilityOfElementLocated(By.id("dataTable1")), "Failed creating course",
+		waitUntil(ExpectedConditions.visibilityOfElementLocated(By.id("dataTable1")), "Failed creating course",
 				2);
 		log.info("Course created correctly");
 
 		// Go to last courses
-		WebElement divTable = browser.getDriver().findElement(By.className("table-responsive"));
+		WebElement divTable = driver.findElement(By.className("table-responsive"));
 		List<WebElement> pagesCourses = divTable.findElements(By.className("page-item"));
 		WebElement lastPageCourses = pagesCourses.get(pagesCourses.size() - 2);
 		lastPageCourses.click();
@@ -126,39 +98,112 @@ public class TestE2EFront extends TestE2E {
 		log.info("Course deleted correctly");
 	}
 
-	@Test
-	public void checkDownload() {
+	/*@Test
+	public void checkDownload(TestInfo testInfo) {
 		// Login
-		this.browser.goToPage("login");
-		this.loginUser(this.browser, "amico", "pass");
+		goToPage("login");
+		loginUser("amico", "pass");
 
 		// Profile
-		this.browser.goToPage("users/amico/profile");
+		goToPage("users/amico/profile");
 
 		// Go to course
-		WebElement inscribedCourses = browser.getDriver().findElement(By.id("inscribed-courses"));
+		WebElement inscribedCourses = driver.findElement(By.id("inscribed-courses"));
 		List<WebElement> buttonsCourses = inscribedCourses.findElements(By.tagName("a"));
 		WebElement firstCourse = buttonsCourses.get(0);
 		firstCourse.click();
 
 		// Go to first subject
-		browser.waitUntil(ExpectedConditions.visibilityOfElementLocated(By.tagName("section")), "Failed opening course",
+		waitUntil(ExpectedConditions.visibilityOfElementLocated(By.tagName("section")), "Failed opening course",
 				2);
-		WebElement subjects = browser.getDriver().findElement(By.tagName("section"));
+		WebElement subjects = driver.findElement(By.tagName("section"));
 		List<WebElement> buttonsSubjects = subjects.findElements(By.tagName("a"));
 		WebElement firstSubject = buttonsSubjects.get(0);
 		firstSubject.click();
 
 		// Download
-		browser.waitUntil(ExpectedConditions.visibilityOfElementLocated(By.className("container-fluid")),
+		waitUntil(ExpectedConditions.visibilityOfElementLocated(By.className("container-fluid")),
 				"Failed opening subject", 2);
-		List<WebElement> files = browser.getDriver().findElements(By.className("item-content"));
+		List<WebElement> files = driver.findElements(By.className("item-content"));
 		WebElement firstFile = files.get(0);
 		firstFile.click();
 
 		// Check download
 		sleep(500);
 		checkDownloadFile();
+	}*/
+
+	public void loginUser(String name, String pass) {
+		// Wait show form login
+		waitUntil(ExpectedConditions.visibilityOfElementLocated(By.name("username")), "No login page", 1);
+
+		// Load form
+		WebElement userField = driver.findElement(By.name("username"));
+		WebElement passField = driver.findElement(By.name("password"));
+
+		WebElement divSubmit = driver.findElement(By.className("form-check"));
+		WebElement submit = divSubmit.findElement(By.tagName("button"));
+
+		// Write credentials
+		userField.sendKeys(name);
+		passField.sendKeys(pass);
+		submit.click();
+
+		// Check login
+		waitUntil(ExpectedConditions.visibilityOfElementLocated(By.className("masthead")), "Login failed", 2);
+
+		WebElement menuBar = driver.findElement(By.id("navbarResponsive"));
+		List<WebElement> elementsBar = menuBar.findElements(By.tagName("a"));
+		assertThat(elementsBar.get(elementsBar.size() - 1).getText()).isEqualToIgnoringCase("MY PROFILE");
+
+		log.info("Loggin successful, user {}", name);
+	}
+
+	public void logout() {
+		WebElement menuBar = driver.findElement(By.id("navbarResponsive"));
+		List<WebElement> elementsBar = menuBar.findElements(By.tagName("a"));
+
+		WebElement buttonLogout;
+		if (elementsBar.size() == 2) {
+			buttonLogout = elementsBar.get(0);
+		} else {
+			buttonLogout = elementsBar.get(1);
+		}
+
+		// Click logout button
+		buttonLogout.click();
+
+		// Check logout
+		menuBar = driver.findElement(By.id("navbarResponsive"));
+		elementsBar = menuBar.findElements(By.tagName("a"));
+
+		assertThat(elementsBar.get(elementsBar.size() - 1).getText()).isEqualToIgnoringCase("LOG IN");
+
+		log.info("Logout successful");
+	}
+
+	public void goToPage() {
+		String url = sutUrl + "new/";
+		
+		this.driver.get(url);
+	}
+
+	public void goToPage(String page) {
+		String url = sutUrl + "new/";
+		
+		this.driver.get(url + page);
+	}
+
+	public void waitUntil(ExpectedCondition<WebElement> expectedCondition, String errorMessage, int seconds) {
+		WebDriverWait waiter = new WebDriverWait(driver, seconds);
+
+		try {
+			waiter.until(expectedCondition);
+		} catch (org.openqa.selenium.TimeoutException timeout) {
+			log.error(errorMessage);
+			throw new org.openqa.selenium.TimeoutException(
+					"\"" + errorMessage + "\" (checked with condition) > " + timeout.getMessage());
+		}
 	}
 
 	private void checkDownloadFile() {
@@ -179,7 +224,7 @@ public class TestE2EFront extends TestE2E {
 	}
 
 	private WebElement getLastCourse() {
-		WebElement divTable = browser.getDriver().findElement(By.id("dataTable1"));
+		WebElement divTable = driver.findElement(By.id("dataTable1"));
 		List<WebElement> trsTable = divTable.findElements(By.tagName("tr"));
 		WebElement lastCourse = trsTable.get(trsTable.size() - 1);
 
---
+++ AMICOServer/src/test/java/com/example/demo/integration/TestAPIRestTemplate.java
@@ -0,0 +1,66 @@
+package com.example.demo.integration;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.integration.SSLClientFactory.HttpClientType;
+
+public class TestAPIRestTemplate extends ElasTestBase{
+
+	final static Logger log = getLogger(lookup().lookupClass());
+
+    protected static String sutUrl;
+    protected static String profile_uri;
+
+    @BeforeAll
+    public static void setupClass() {
+        String sutHost = System.getenv("ET_SUT_HOST");
+        String sutPort = System.getenv("ET_SUT_PORT");
+        String sutProtocol = System.getenv("ET_SUT_PROTOCOL");
+
+        if (sutHost == null) {
+            sutUrl = "https://localhost:8443/";
+            profile_uri = sutUrl + "api/users/{user}";
+        } else {
+            sutPort = sutPort != null ? sutPort : "8080";
+            sutProtocol = sutProtocol != null ? sutProtocol : "http";
+
+            sutUrl = sutProtocol + "://" + sutHost + ":" + sutPort;
+        }
+        
+        logger.info("Webapp URL: " + sutUrl);
+    }
+    
+	@Test
+	public void checkShowProfile() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+		String url = profile_uri.replace("{user}", "amico");
+		RestTemplate restTemplate = new RestTemplate(SSLClientFactory.getClientHttpRequestFactory(HttpClientType.HttpClient));
+
+		HttpStatus status;
+		HttpStatus expected = HttpStatus.UNAUTHORIZED;
+
+		try {
+			ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+			status = response.getStatusCode();
+		} catch (HttpStatusCodeException e) {
+			status = e.getStatusCode();
+		}
+
+		Assert.assertEquals("failure - expected HTTP status " + expected, expected, status);
+		log.info("The response is correct");
+	}
+
+}
