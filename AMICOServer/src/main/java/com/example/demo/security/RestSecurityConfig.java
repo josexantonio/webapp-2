a8ff68e96de75eec32486da65a4771f9843a5f73
Unknown <natalia.madruenno@gmail.com>
2019-04-30 10:18:25 +0200

    Bug reparado

--- AMICOServer/src/main/java/com/example/demo/security/RestSecurityConfig.java
+++ AMICOServer/src/main/java/com/example/demo/security/RestSecurityConfig.java
@@ -34,10 +34,9 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/courses/img/**").permitAll();
 		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN");
 		// Courses (PENDING)
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/courses/**").hasRole("USER");
 		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/courses/user/**").hasRole("USER");
 		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/courses/admin/**").hasRole("ADMIN");
-		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/courses/**").hasRole("AMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/courses/name/**").hasRole("ADMIN");
 		// Moodle
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/moodle/**").hasRole("USER");
 		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/moodle/**").hasRole("USER");
